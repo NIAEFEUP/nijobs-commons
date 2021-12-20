0d94eea83891b5e8617e38ca8a9d9c0a63af4cd0
Nuno Pereira <nunoafonso2002@gmail.com>
2021-12-20 20:33:53 +0000

    feat(pencil): added CompanyApplication constants

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as Offer} from "./Offer.js";
 export {default as Company} from "./Company.js";
 export {default as Account} from "./Account.js";
+export {default as CompanyApplication} from "./CompanyApplication.js";

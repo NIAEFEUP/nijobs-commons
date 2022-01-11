10be6298962325f93a1ae600e3a92c2f029a3d31
Nuno Pereira <nunoafonso2002@gmail.com>
2022-01-11 12:49:02 +0000

    feat(pencil): added TechnologyTypes constants

---
+++ src/TechnologyTypes.js
@@ -0,0 +1,29 @@
+const TechnologyTypes = Object.freeze([
+    "React",
+    "Angular",
+    "Vue",
+    "Node.js",
+    "Java",
+    "C++",
+    "C",
+    "C#",
+    "Clojure",
+    "Go",
+    "Haskell",
+    "Spring Boot",
+    "Android",
+    "Flutter",
+    "Dart",
+    "PHP",
+    "CSS",
+    "Other",
+]);
+
+const MIN_TECHNOLOGIES = 1;
+const MAX_TECHNOLOGIES = 7;
+
+export default Object.freeze({
+    TechnologyTypes,
+    MAX_TECHNOLOGIES,
+    MIN_TECHNOLOGIES
+});
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export {default as Offer} from "./Offer.js";
 export {default as Company} from "./Company.js";
 export {default as Account} from "./Account.js";
 export {default as CompanyApplication} from "./CompanyApplication.js";
+export {default as TechnologyTypes} from "./TechnologyTypes.js";

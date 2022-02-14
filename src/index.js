1c950c0e77cd01e65d5f17f579722e4040214b6c
Nuno Pereira <nunoafonso2002@gmail.com>
2022-02-14 23:16:50 +0000

    feat(pencil): Added ApplicationStatus constants

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export {default as CompanyApplication} from "./CompanyApplication.js";
 export {default as TechnologyTypes} from "./TechnologyTypes.js";
 export {default as JobTypes} from "./JobTypes.js";
 export {default as FieldTypes} from "./FieldTypes.js";
+export {default as ApplicationStatus} from "./ApplicationStatus.js";

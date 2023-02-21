c70de329b11cd1aa6ce3d75b02524740f8be4a44
Nuno Pereira <nunoafonso2002@gmail.com>
2023-02-21 19:54:55 +0000

    feat(pencil): added time constants

---
+++ src/TimeConstants.js
@@ -0,0 +1,6 @@
+export const SECOND_IN_MS = 1000;
+export const MINUTE_IN_MS = 60 * SECOND_IN_MS;
+export const HOUR_IN_MS = 60 * MINUTE_IN_MS;
+export const DAY_IN_MS = 24 * HOUR_IN_MS;
+export const MONTH_IN_MS = 30.42 * DAY_IN_MS;
+export const OFFER_MAX_LIFETIME_MONTHS = 6;
